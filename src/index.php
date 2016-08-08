75008e8eca6a7c7cf06cc772997edec4fd7725bf
rahulnatarajan <rahulna@microland.com>
2016-08-08 11:27:43 +0530

    BUILD 30

--- src/index.php
+++ src/index.php
@@ -7,7 +7,7 @@
         <title>Simple PHP App</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link href="assets/css/bootstrap.min.css" rel="stylesheet">
-        <style>body {margin-top: 40px; background-color: #B15885;}</style>
+        <style>body {margin-top: 40px; background-color: #8CCC29;}</style>
         <link href="assets/css/bootstrap-responsive.min.css" rel="stylesheet">
         <!--[if lt IE 9]><script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
     </head>
@@ -17,7 +17,7 @@
             <div class="hero-unit">
                 <h1>Microland COI Simple PHP Application running on Docker</h1>
                 <h2>Congratulations!!!</h2>
-                <p>Your PHP application is now running on a container in MLCOI CASS - BUILD 29.</p>
+                <p>Your PHP application is now running on a container in MLCOI CASS - BUILD 30.</p>
                 <p>The container is running PHP version - Its Awesome!!!<?php echo phpversion(); ?>.</p>
                 <?php
                         $myfile = fopen("/var/www/my-vol/date", "r") or die("");
